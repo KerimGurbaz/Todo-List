cab1ec4d3a9d994b1f6d30f77ac5e4767c9b423e
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-17 01:36:32 +0200

    updated

--- app.js
+++ app.js
@@ -2,27 +2,34 @@ const plus = document.querySelector(".plus");
 const main = document.querySelector(".main");
 // const todo = document.querySelector(".todo").value;
 let para ;
-todo = document.querySelector(".todo");
+let todo = document.querySelector(".todo");
 
 
 
 
 
 
-// main.addEventListener("click", (e) =>{
+main.addEventListener("click", (e) =>{
+    console.log(e.target)
+
+ if(e.target.classList.contains("plus")){
+   const newUl = document.createElement("ul");
+    let newLi = document.createElement("li");
+    newLi.style.listStyle = "none";
+    newLi.style.backgroundColor ="red"
+    newLi.style.color ="white"
     
+    newUl.style.flex
+    const trash = `<i class="fa-solid fa-trash-can"></i>`
+    const space = "    ";
+   
+    newLi.innerHTML = `<input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">${space}${todo.value}${trash}` ;
+    newUl.appendChild(newLi);
+    main.appendChild(newUl);
+ }
+})
 
-//  if(e.target.classList.contains("plus")){
-//     para = document.createElement("div");
-//     const trash = `<i class="fa-solid fa-trash-can"></i>`
-
-//     para.innerHTML = `${todo.value}`;
-//     main.appendChild(para);
-//     main.appendChild(trash);
-//     para.style.color ="red";
-//     para.style.backgroundColor ="green"
 
 
-    
-//  }
-// })
+
+
