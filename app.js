ed4a98b6d1bd99131e9eb2f9f3a16a6d7599cbc5
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-22 00:18:03 +0200

    updated

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const todo = document.querySelector(".todo");
 const plus = document.querySelector(".fa-solid");
 const main = document.querySelector(".main");
-const section = document.querySelector(".section");
+
 
 
 main.addEventListener("click", (e) =>{
    if(e.target.classList.contains("plus")){
       const newLi = document.createElement("li");
-      newLi.style.listStyle = "none"
-      section.appendChild(newLi);
-
-
-      newLi.innerHTML = `<i class="fa-solid fa-check"></i> ${todo.value} <i class="fa-solid fa-trash-can"></i>`
-      
+      newLi.style.listStyle = "none";
+      main.appendChild(newLi);
+      newLi.innerHTML = `<i class="fa-solid fa-check"></i> ${todo.value} <i class="fa-solid fa-trash-can"></i>`;  
       todo.value= " ";
-
-   }
-
+} else if(e.target.classList.contains("fa-check") && e.target.classList.contains("myStyle")){
+   console.log("first")
+}
+else{
+   "ben secilim"
+}
 })
+
+
+
+
 
