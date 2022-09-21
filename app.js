aa0021235bf2c55315bc1a4ae37d25d9ca853985
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-21 23:17:47 +0200

    updated

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+const todo = document.querySelector(".todo");
+const plus = document.querySelector(".fa-solid");
+const main = document.querySelector(".main");
+const section = document.querySelector(".section");
 
+
+main.addEventListener("click", (e) =>{
+   if(e.target.classList.contains("plus")){
+      const newLi = document.createElement("li");
+      newLi.style.listStyle = "none"
+      section.appendChild(newLi);
+
+
+      newLi.innerHTML = `<i class="fa-solid fa-check"></i> ${todo.value} <i class="fa-solid fa-trash-can"></i>`
+      
+      todo.value= " ";
+
+   }
+
+})
 
