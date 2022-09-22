6873fe046c123b440d9e7e0c637513c32742a58d
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-22 11:12:10 +0200

    updated

--- app.js
+++ app.js
@@ -10,18 +10,26 @@ main.addEventListener("click", (e) =>{
       const newLi = document.createElement("li");
       newLi.style.listStyle = "none";
       main.appendChild(newLi);
-      newLi.innerHTML = `<i class="fa-solid fa-check"></i> ${todo.value} <i class="fa-solid fa-trash-can"></i>`; 
+      newLi.innerHTML = `<i class="fa-solid fa-check"></i>
+      <p> ${todo.value}</p>
+       <i class="fa-solid fa-trash-can"></i>`; 
+       
       
       todo.value= " ";
 } else if(e.target.classList.contains("fa-check") && !e.target.classList.contains("myStyle")){
    e.target.classList.add("myStyle");
-
-   
-}else{
-   e.target.classList.remove("myStyle");
+  e.target.nextElementSibling.style.textDecoration = "line-through";
    
 }
-
+else if(e.target.classList.contains("fa-trash-can")){
+   console.log(e.target.parentElement.remove())
+}
+else{
+   e.target.classList.remove("myStyle");
+   e.target.nextElementSibling.style.textDecoration = "none";
+   
+   
+}
 })
 
 
